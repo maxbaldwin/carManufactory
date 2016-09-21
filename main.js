c4fd141cdf59a096510ea6137e45f707d63352f4
Max Baldwin <maxrbaldwin@gmail.com>
2016-09-20 19:33:05 -0500

    finished up blank files

---
+++ main.js
@@ -0,0 +1,53 @@
+/*
+  Create a function named Car with one property named `manufactured_date`
+  whose value will be Date.now()
+ */
+function Car () {
+  // Create the manufactured date property (see above)
+}
+
+/*
+  Create a constructor function named Make with one 
+  argument that will hold the manufacturer of the Make (see below). 
+
+  Set its prototype to a new instance of the Car function.
+
+  Define a property `manufacturer` and assign the 
+  manufacturer property to the argument value.
+ */
+function Make (maker) {
+  this.manufacturer = maker;
+}
+// Set the prototype to Car
+
+/*
+  1. Create a new constructor function for the make of your favorite car.
+  2. It must inherit from the general Make function.
+  3. It must have one argument that will to hold the model name.
+  4. The constructor must define a property to hold the model name.
+ */
+function YourFavoriteMake (name) {
+  this.modelName = name;
+}
+YourFavoriteMake.prototype = new Make(...);
+
+/*
+    1. Create a constructor function named after your favorite car model.
+    2. It must inherit from the specific make type you created in previous step.
+    3. When creating the inheritance relationship, pass in the model name
+       to the make constructor function
+*/
+function YourFavoriteModel () {
+
+}
+// Set the prototype to appropriate model you created above and set the model name argument
+YourFavoriteModel.prototype = new YourFavoriteMake(...);
+
+/*
+    Being an avid car collector, you own three different cars all of
+    same model from your favorite manufacturer.
+
+    1. Create three new instances of your favorite car.
+    2. Create a new property on each object to hold the license plate
+       number for each car.
+*/
